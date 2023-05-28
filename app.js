869ad2550d4f1ae9296f1cf983b515d584bf87f8
Woohyeon Roh <ykjrco@naver.com>
2023-05-28 07:14:32 +0000

    Update: mouse painting function

--- app.js
+++ app.js
@@ -1,6 +1,41 @@
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
 
+let isPainting = false;
+
 canvas.width = 800;
 canvas.height = 800;
+ctx.lineWidth = 2;
 
+const colors = [
+    "#ff3838",
+    "#ffb8b8",
+    "#c56cf0",
+    "#ff9f1a",
+    "#fff200",
+    "#32ff7e",
+    "#7efff5"
+];
+
+function onMove(event){
+    if (isPainting) {
+        ctx.lineTo(event.offsetX, event.offsetY);
+        ctx.stroke();
+        return;
+    }
+    ctx.moveTo(event.offsetX, event.offsetY);
+}
+
+function startPainting() {
+    isPainting = true;
+
+}
+
+function cancelPainting() {
+    isPainting = false;
+}
+
+canvas.addEventListener("mousemove", onMove);
+canvas.addEventListener("mousedown", startPainting);
+canvas.addEventListener("mouseup", cancelPainting);
+canvas.addEventListener("mouseleave", cancelPainting);
